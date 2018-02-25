db58738c6d9b616519c63aad142a210dfe00914c
ChrissieFernando <chrissiefdo002@gmail.com>
2018-02-25 11:56:28 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  list: Array<any> = [];
+ list: Array<any> = [];
+  id: Number;
+  toggle: Boolean = false;
   constructor(private service: TodoService) {
 
   }
@@ -20,8 +22,21 @@ export class AppComponent implements OnInit {
   onClick(user, checkBox) {
     user.completed = checkBox.checked;
   }
-   onRemove(user) {
+  onRemove(user) {
     const index = this.list.indexOf(user);
     this.list.splice(index, 1);
+  }
+  onEdit(user) {
+    user.disabled = false;
+   user.toggle = !user.toggle;
+   user.save = 'save';
+    this.id = user.id;
+  }
+
+  onSave(user, title, checkBox) {
+    user.save = 'updated';
+    user.title = title.value;
+    user.disabled = true;
+    user.completed = checkBox.checked;
   }
 }
